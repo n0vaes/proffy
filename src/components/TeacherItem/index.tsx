876e59353b5930ccacb70328d33ed2ab4dce2ffc
Alan Novaes <alanovaes@live.com>
2020-08-08 08:36:32 -0300

    web final

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,35 +1,56 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api';
 
 import './styles.css'
 
-const TeacherItem = () => {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+};
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  const createNewConnection = () => {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/2254731?s=400&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4" alt="Diego Fernandes"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Diego Fernandes</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
       <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br /><br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200 mil pessoas passaram pelas minhas explosões.
+      {teacher.bio}
       </p>
 
 
       <footer>
         <p> 
           Preço/hora
-          <strong>R$ 80,00</strong> 
+          <strong>R$ {teacher.cost}</strong> 
         </p>
-        <button type="button">
+        <a target="_blank" 
+        onClick={createNewConnection} 
+        href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
